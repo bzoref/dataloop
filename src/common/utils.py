825f2fa001eecf9adafe02ac01d8ff4f53acb2cd
Boaz Zoref <bzoref.adv@gmail.com>
2023-03-14 19:17:00 +0200

    Added an annotation calculation method

--- src/common/utils.py
+++ src/common/utils.py
@@ -30,3 +30,25 @@ def get_annotation_info(item_id, token):
     return json.loads(response.content)
 
 
+def calculate_annotation_starting_and_ending_point(image_container_data):
+    image_start_x_point = round(image_container_data['x'])
+    image_start_y_point = round(image_container_data['y'])
+    image_width = round(image_container_data['width'])
+    image_width_quarter = image_width/4
+    image_height = round(image_container_data['height'])
+    image_height_quarter = image_height/4
+    annotation_x_start_point = round(image_start_x_point + image_width_quarter)
+    annotation_y_start_point = round(image_start_y_point + image_height_quarter)
+    annotation_x_end_point = round(image_start_x_point + 3*image_width_quarter)
+    annotation_y_end_point = round(image_start_y_point + 3*image_height_quarter)
+    return dict(
+        annotation_x_start_point=annotation_x_start_point,
+        annotation_y_start_point=annotation_y_start_point,
+        annotation_x_end_point=annotation_x_end_point,
+        annotation_y_end_point=annotation_y_end_point
+    )
+
+
+
+
+
